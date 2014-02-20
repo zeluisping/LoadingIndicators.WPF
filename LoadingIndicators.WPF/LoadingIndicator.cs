8a22c7840ca1c8d805540045dc07c629b3453bb7
100GPing100 <zeluis.100@gmail.com>
2014-02-20 23:45:20 +0000

    Initial commit

---
+++ LoadingIndicators.WPF/LoadingIndicator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+
+namespace LoadingIndicators.WPF
+{
+    [TemplatePart(Name = "Border", Type = typeof(Border))]
+    public class LoadingIndicator : Control
+    {
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            Border border = (Border) GetTemplateChild("PART_Border");
+            VisualStateManager.GoToElementState(border, "Active", false);
+        }
+
+        // Constructors
+        public LoadingIndicator()
+        {
+        }
+    }
+}
