665726eb565d74e0d1699f5f5fe26101ae34ee53
100GPing100 <zeluis.100@gmail.com>
2014-02-21 21:56:28 +0000

    Styles.xaml, LoadingRing

--- LoadingIndicators.WPF/LoadingIndicator.cs
+++ LoadingIndicators.WPF/LoadingIndicator.cs
@@ -8,9 +8,31 @@ using System.Windows.Shapes;
 
 namespace LoadingIndicators.WPF
 {
+    /// <summary>
+    /// A control featuring a range of loading indicating animations.
+    /// </summary>
     [TemplatePart(Name = "Border", Type = typeof(Border))]
     public class LoadingIndicator : Control
     {
+        /// <summary>
+        /// Identifies the <see cref="LoadingIndicators.WPF.LoadingIndicator.SpeedRatio"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty SpeedRatioProperty =
+            DependencyProperty.Register("SpeedRatio", typeof(double), typeof(LoadingIndicator), new PropertyMetadata(1));
+
+        /// <summary>
+        /// Get/set the speed ratio of the animation.
+        /// </summary>
+        public double SpeedRatio
+        {
+            get { return (double) GetValue(SpeedRatioProperty); }
+            set { SetValue(SpeedRatioProperty, value); }
+        }
+
+        /// <summary>
+        /// When overridden in a derived class, is invoked whenever application code
+        /// or internal processes call System.Windows.FrameworkElement.ApplyTemplate().
+        /// </summary>
         public override void OnApplyTemplate()
         {
             base.OnApplyTemplate();
@@ -19,7 +41,9 @@ namespace LoadingIndicators.WPF
             VisualStateManager.GoToElementState(border, "Active", false);
         }
 
-        // Constructors
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LoadingIndicators.WPF.LoadingIndicator"/> class.
+        /// </summary>
         public LoadingIndicator()
         {
         }
